4a82b1716fe4e1026849254dcb8d3c4dbe6afea6
Chance Phiniezy <phiniezyc@gmail.com>
2018-11-03 01:52:15 -0400

    Adds boilerplale server.js file for backend

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+
+app.get("/test", (req, res) => {
+    res.json("yo!!!");
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/'));
+});
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
